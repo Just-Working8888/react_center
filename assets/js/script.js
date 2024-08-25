4bd2c25f84b873262bbc601b22747320d1ca5767
Bekbolot <justworking8888@gmail.com>
2024-08-25 20:21:13 +0600

    sex

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,12 +21,43 @@ const swiper = new Swiper('.swiper-container', {
     },
 });
 const swiper2 = new Swiper('.swiper-container2', {
-    // Optional parameters
     direction: 'horizontal',
     loop: true,
-    slidesPerView: 3.5, // Показывать 2.5 слайда одновременно
+    slidesPerView: 3.5, // Показывать 3.5 слайда по умолчанию
     centeredSlides: true, // Центрировать активный слайд
     spaceBetween: 30, // Отступ между слайдами
 
+    breakpoints: {
+        1079: { // Для экранов шириной 1079px и меньше
+            slidesPerView: 1, // Показывать 1 слайд
+            centeredSlides: false, // Отключить центрирование
+            spaceBetween: 10, // Уменьшить отступы
+        },
+        1080: { // Для экранов шириной 1080px и больше
+            slidesPerView: 3.5, // Показывать 3.5 слайда
+            centeredSlides: true,
+            spaceBetween: 30,
+        }
+    }
+});
 
+const swiper3 = new Swiper('.swiper-container3', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1.3, // Показывать 3.5 слайда по умолчанию
+    centeredSlides: true, // Центрировать активный слайд
+    spaceBetween: 30, // Отступ между слайдами
+
+    breakpoints: {
+        1079: { // Для экранов шириной 1079px и меньше
+            slidesPerView: 1, // Показывать 1 слайд
+            centeredSlides: false, // Отключить центрирование
+            spaceBetween: 10, // Уменьшить отступы
+        },
+        1080: { // Для экранов шириной 1080px и больше
+            slidesPerView: 3.5, // Показывать 3.5 слайда
+            centeredSlides: true,
+            spaceBetween: 30,
+        }
+    }
 });
