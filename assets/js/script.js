baeaee85a3d63e0081e939ca2139071b622ff17a
Bekbolot <justworking8888@gmail.com>
2024-08-29 15:11:45 +0600

    sex

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,14 +23,14 @@ const swiper = new Swiper('.swiper-container', {
 const swiper2 = new Swiper('.swiper-container2', {
     direction: 'horizontal',
     loop: true,
-    slidesPerView: 3.5, // Показывать 3.5 слайда по умолчанию
+    slidesPerView: 1.5, // Показывать 3.5 слайда по умолчанию
     centeredSlides: true, // Центрировать активный слайд
     spaceBetween: 30, // Отступ между слайдами
 
     breakpoints: {
         1079: { // Для экранов шириной 1079px и меньше
-            slidesPerView: 1, // Показывать 1 слайд
-            centeredSlides: false, // Отключить центрирование
+            slidesPerView: 1.5, // Показывать 1 слайд
+            centeredSlides: true, // Отключить центрирование
             spaceBetween: 10, // Уменьшить отступы
         },
         1080: { // Для экранов шириной 1080px и больше
@@ -60,4 +60,39 @@ const swiper3 = new Swiper('.swiper-container3', {
             spaceBetween: 30,
         }
     }
+});
+
+const swiper4 = new Swiper('.swiper-container4', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1.3, // Показывать 3.5 слайда по умолчанию
+    centeredSlides: true, // Центрировать активный слайд
+    spaceBetween: 30, // Отступ между слайдами
+
+    breakpoints: {
+        1079: { // Для экранов шириной 1079px и меньше
+            slidesPerView: 1.3, // Показывать 1 слайд
+            centeredSlides: false, // Отключить центрирование
+            spaceBetween: 10, // Уменьшить отступы
+        },
+        1080: { // Для экранов шириной 1080px и больше
+            slidesPerView: 4, // Показывать 3.5 слайда
+            centeredSlides: false,
+            spaceBetween: 30,
+        }
+    }
+});
+
+
+
+const mobilebtn = document.getElementById("mobilebtn");
+const closebtn = document.getElementById("closebtn");
+const mobile_nav = document.getElementById("mobile_nav");
+
+mobilebtn.addEventListener("click", () => {
+    mobile_nav.classList.add("open"); // Открыть меню
+});
+
+closebtn.addEventListener("click", () => {
+    mobile_nav.classList.remove("open"); // Закрыть меню
 });
